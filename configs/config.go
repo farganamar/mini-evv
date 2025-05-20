90f5fb8521778b6391b9a1e44f197e851831981b
farganzorahealth <fargan.amar@zorahealth.co>
2025-05-21 06:36:20 +0700

    refactor: remove viper dependency and enhance environment variable handling in config loading

--- configs/config.go
+++ configs/config.go
@@ -1,81 +1,81 @@
 package configs
 
 import (
+	"os"
+	"strconv"
 	"strings"
 	"sync"
 
 	"github.com/rs/zerolog/log"
-	"github.com/spf13/viper"
 )
 
 // Config is a struct that will receive configuration options via environment
 // variables.
-
 type Config struct {
 	App struct {
 		CORS struct {
-			AllowCredentials bool     `mapstructure:"ALLOWCREDENTIALS"`
-			AllowedHeaders   []string `mapstructure:"ALLOWEDHEADERS"`
-			AllowedMethods   []string `mapstructure:"ALLOWEDMETHODS"`
-			AllowedOrigins   []string `mapstructure:"ALLOWEDORIGINS"`
-			Enable           bool     `mapstructure:"ENABLE"`
-			MaxAgeSeconds    int      `mapstructure:"MAXAGESECONDS"`
-		} `mapstructure:"cors"`
-		Name string `mapstructure:"NAME"`
-		Tz   string `mapstructure:"TZ"`
-		URL  string `mapstructure:"URL"`
-	} `mapstructure:"APP"`
+			AllowCredentials bool
+			AllowedHeaders   []string
+			AllowedMethods   []string
+			AllowedOrigins   []string
+			Enable           bool
+			MaxAgeSeconds    int
+		}
+		Name string
+		Tz   string
+		URL  string
+	}
 	DB struct {
 		Postgres struct {
 			Read struct {
-				Host     string `mapstructure:"HOST"`
-				Port     string `mapstructure:"PORT"`
-				User     string `mapstructure:"USER"`
-				Password string `mapstructure:"PASSWORD"`
-				Name     string `mapstructure:"NAME"`
-				Timezone string `mapstructure:"TIMEZONE"`
-			} `mapstructure:"read"`
+				Host     string
+				Port     string
+				User     string
+				Password string
+				Name     string
+				Timezone string
+			}
 			Write struct {
-				Host     string `mapstructure:"HOST"`
-				Port     string `mapstructure:"PORT"`
-				User     string `mapstructure:"USER"`
-				Password string `mapstructure:"PASSWORD"`
-				Name     string `mapstructure:"NAME"`
-				Timezone string `mapstructure:"TIMEZONE"`
-			} `mapstructure:"WRITE"`
-		} `mapstructure:"POSTGRES"`
+				Host     string
+				Port     string
+				User     string
+				Password string
+				Name     string
+				Timezone string
+			}
+		}
 		SQLite struct {
-			Path string `mapstructure:"PATH"`
+			Path string
 		}
-	} `mapstructure:"DB"`
+	}
 	Server struct {
-		Env      string `mapstructure:"ENV"`
-		LogLevel string `mapstructure:"LOGLEVEL"`
-		Port     string `mapstructure:"PORT"`
+		Env      string
+		LogLevel string
+		Port     string
 		Shutdown struct {
-			CleanupPeriodSeconds int64 `mapstructure:"CLEANUP_PERIOD_SECONDS"`
-			GracePeriodSeconds   int64 `mapstructure:"GRACE_PERIOD_SECONDS"`
+			CleanupPeriodSeconds int64
+			GracePeriodSeconds   int64
 		}
-	} `mapstructure:"server"`
+	}
 	Cache struct {
 		Redis struct {
 			Primary struct {
-				Host       string `mapstructure:"HOST"`
-				Port       string `mapstructure:"PORT"`
-				Password   string `mapstructure:"PASSWORD"`
-				Prefix     string `mapstructure:"PREFIX"`
-				RetryCount int    `mapstructure:"RETRY_COUNT"`
+				Host       string
+				Port       string
+				Password   string
+				Prefix     string
+				RetryCount int
 			}
 		}
-	} `mapstructure:"cache"`
+	}
 	AccessToken struct {
-		ExpiryInHour int    `mapstructure:"EXPIRYINHOUR"`
-		Secret       string `mapstructure:"SECRET"`
-	} `mapstructure:"ACCESSTOKEN"`
+		ExpiryInHour int
+		Secret       string
+	}
 	RefreshToken struct {
-		ExpiryInHour int    `mapstructure:"EXPIRYINHOUR"`
-		Secret       string `mapstructure:"SECRET"`
-	} `mapstructure:"REFRESHTOKEN"`
+		ExpiryInHour int
+		Secret       string
+	}
 }
 
 var (
@@ -83,67 +83,109 @@ var (
 	once sync.Once
 )
 
-// Get are responsible to load env and get data an return the struct
+// Get loads configuration from environment variables
 func Get() *Config {
-	// Set up automatic environment variable reading
-	viper.AutomaticEnv()
-
-	// Try to read from .env file, but don't fail if it doesn't exist
-	viper.SetConfigFile(".env")
-	if err := viper.ReadInConfig(); err != nil {
-		// If .env file doesn't exist or can't be read, just log it and continue
-		// using environment variables
-		log.Info().Msg("No .env file found or couldn't be read, using environment variables")
-	} else {
-		log.Info().Msg("Config loaded from .env file")
-	}
-
-	// Set up mappings between environment variables and config fields
-	// This ensures that environment variables are properly mapped to struct fields
-	viper.SetEnvPrefix("") // No prefix for environment variables
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-
-	// Print out all keys Viper knows about
-	for _, key := range viper.AllKeys() {
-		val := viper.GetString(key)
-		newKey := strings.ReplaceAll(key, "_", ".")
-		viper.Set(newKey, val)
-	}
-
 	once.Do(func() {
-		log.Info().Msg("Service configuration initialized.")
-		err := viper.Unmarshal(&conf)
-		if err != nil {
-			log.Fatal().Err(err).Msg("Failed unmarshall config")
-		}
+		log.Info().Msg("Loading configuration from environment variables")
+
+		// App CORS config
+		conf.App.CORS.AllowCredentials = getBool("APP_CORS_ALLOWCREDENTIALS", true)
+		conf.App.CORS.AllowedHeaders = getStringSlice("APP_CORS_ALLOWEDHEADERS", []string{"Accept", "Authorization", "Content-Type"})
+		conf.App.CORS.AllowedMethods = getStringSlice("APP_CORS_ALLOWEDMETHODS", []string{"GET", "POST", "PUT", "DELETE"})
+		conf.App.CORS.AllowedOrigins = getStringSlice("APP_CORS_ALLOWEDORIGINS", []string{"*"})
+		conf.App.CORS.Enable = getBool("APP_CORS_ENABLE", true)
+		conf.App.CORS.MaxAgeSeconds = getInt("APP_CORS_MAXAGESECONDS", 300)
+
+		// App config
+		conf.App.Name = getEnv("APP_NAME", "mini-evv")
+		conf.App.Tz = getEnv("APP_TZ", "UTC")
+		conf.App.URL = getEnv("APP_URL", "http://localhost:3200")
+
+		// DB SQLite config
+		conf.DB.SQLite.Path = getEnv("DB_SQLITE_PATH", "db/mini-evv.db")
+
+		// Server config
+		conf.Server.Env = getEnv("SERVER_ENV", "development")
+		conf.Server.LogLevel = getEnv("SERVER_LOGLEVEL", "info")
+		conf.Server.Port = getEnv("SERVER_PORT", "3200")
+		conf.Server.Shutdown.CleanupPeriodSeconds = getInt64("SERVER_SHUTDOWN_CLEANUP_PERIOD_SECONDS", 15)
+		conf.Server.Shutdown.GracePeriodSeconds = getInt64("SERVER_SHUTDOWN_GRACE_PERIOD_SECONDS", 15)
+
+		// Cache Redis config
+		conf.Cache.Redis.Primary.Host = getEnv("CACHE_REDIS_PRIMARY_HOST", "")
+		conf.Cache.Redis.Primary.Port = getEnv("CACHE_REDIS_PRIMARY_PORT", "6379")
+		conf.Cache.Redis.Primary.Password = getEnv("CACHE_REDIS_PRIMARY_PASSWORD", "")
+		conf.Cache.Redis.Primary.Prefix = getEnv("CACHE_REDIS_PRIMARY_PREFIX", "mini-evv:")
+		conf.Cache.Redis.Primary.RetryCount = getInt("CACHE_REDIS_PRIMARY_RETRY_COUNT", 3)
+
+		// Token configs
+		conf.AccessToken.ExpiryInHour = getInt("ACCESSTOKEN_EXPIRYINHOUR", 1)
+		conf.AccessToken.Secret = getEnv("ACCESSTOKEN_SECRET", "access-token-secret")
+		conf.RefreshToken.ExpiryInHour = getInt("REFRESHTOKEN_EXPIRYINHOUR", 24*7) // 1 week
+		conf.RefreshToken.Secret = getEnv("REFRESHTOKEN_SECRET", "refresh-token-secret")
+
+		log.Info().Msg("Configuration loaded successfully")
 	})
 
 	return &conf
 }
 
-// func Get() *Config {
-// 	viper.SetConfigFile(".env")
-// 	viper.AutomaticEnv()
+// Helper functions to get environment variables with default values
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
 
-// 	err := viper.ReadInConfig()
-// 	if err != nil {
-// 		log.Fatal().Err(err).Msg("Failed reading config file")
-// 	}
+func getBool(key string, defaultValue bool) bool {
+	strValue := os.Getenv(key)
+	if strValue == "" {
+		return defaultValue
+	}
 
-// 	// Print out all keys Viper knows about
-// 	for _, key := range viper.AllKeys() {
-// 		val := viper.GetString(key)
-// 		newKey := strings.ReplaceAll(key, "_", ".")
-// 		viper.Set(newKey, val)
-// 	}
+	boolValue, err := strconv.ParseBool(strValue)
+	if err != nil {
+		log.Warn().Str("key", key).Str("value", strValue).Msg("Invalid boolean value, using default")
+		return defaultValue
+	}
+	return boolValue
+}
 
-// 	once.Do(func() {
-// 		log.Info().Msg("Service configuration initialized.")
-// 		err = viper.Unmarshal(&conf)
-// 		if err != nil {
-// 			log.Fatal().Err(err).Msg("Failed unmarshall config")
-// 		}
-// 	})
+func getInt(key string, defaultValue int) int {
+	strValue := os.Getenv(key)
+	if strValue == "" {
+		return defaultValue
+	}
 
-// 	return &conf
-// }
+	intValue, err := strconv.Atoi(strValue)
+	if err != nil {
+		log.Warn().Str("key", key).Str("value", strValue).Msg("Invalid integer value, using default")
+		return defaultValue
+	}
+	return intValue
+}
+
+func getInt64(key string, defaultValue int64) int64 {
+	strValue := os.Getenv(key)
+	if strValue == "" {
+		return defaultValue
+	}
+
+	int64Value, err := strconv.ParseInt(strValue, 10, 64)
+	if err != nil {
+		log.Warn().Str("key", key).Str("value", strValue).Msg("Invalid int64 value, using default")
+		return defaultValue
+	}
+	return int64Value
+}
+
+func getStringSlice(key string, defaultValue []string) []string {
+	strValue := os.Getenv(key)
+	if strValue == "" {
+		return defaultValue
+	}
+
+	return strings.Split(strValue, ",")
+}
